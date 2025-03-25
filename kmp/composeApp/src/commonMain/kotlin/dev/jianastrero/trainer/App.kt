1f249ba1e6ee10b9a9ba58617e4c27b464724860
Jian James Astrero <jian.astrero@firstdigitalfinance.com>
2025-03-26 06:00:56 +0800

    Add Pokemon Type Icons and Colors
    Adds new drawable resources for various Pokemon types (grass, ghost, flying, fire, fighting, fairy, electric, dragon, water, steel, rock, psychic, poison, normal, ice, ground, dark, bug) and integrates them into the application. Also includes a color association for each type. The app's UI has been updated to display the list of Pokemon Types with their associated icons and colors.

--- kmp/composeApp/src/commonMain/kotlin/dev/jianastrero/trainer/App.kt
+++ kmp/composeApp/src/commonMain/kotlin/dev/jianastrero/trainer/App.kt
@@ -4,9 +4,14 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
@@ -14,6 +19,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import dev.jianastrero.trainer.domain.enumeration.PokemonType
 import dev.jianastrero.trainer.ui.atom.TrainerButton
 import dev.jianastrero.trainer.ui.effect.SystemBarIconColorEffect
 import dev.jianastrero.trainer.ui.theme.TrainerTheme
@@ -22,6 +33,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import trainer.composeapp.generated.resources.Res
 import trainer.composeapp.generated.resources.compose_multiplatform
+import trainer.composeapp.generated.resources.fighting
 
 @Composable
 @Preview
@@ -41,6 +53,35 @@ fun App() {
                 text = "Hello world!",
                 onClick = { showContent = !showContent }
             )
+            LazyColumn(modifier = Modifier.fillMaxWidth().weight(1f)) {
+                items(PokemonType.entries) { pokemonType ->
+                    Row(
+                        modifier = Modifier
+                            .background(pokemonType.color)
+                            .fillMaxWidth()
+                            .padding(12.dp)
+                    ) {
+                        if (pokemonType.icon != null) {
+                            Image(
+                                painter = painterResource(pokemonType.icon),
+                                contentDescription = null,
+                                modifier = Modifier.size(36.dp)
+                            )
+                        }
+                        Text(
+                            text = pokemonType.name,
+                            style = TextStyle(
+                                color = Color.White,
+                                shadow = Shadow(
+                                    color = Color.Black,
+                                    offset = Offset(4f, 4f),
+                                    blurRadius = 4f
+                                )
+                            )
+                        )
+                    }
+                }
+            }
         }
     }
 }
--- kmp/composeApp/src/commonMain/kotlin/dev/jianastrero/trainer/domain/enumeration/PokemonType.kt
+++ kmp/composeApp/src/commonMain/kotlin/dev/jianastrero/trainer/domain/enumeration/PokemonType.kt
@@ -1,30 +1,54 @@
 package dev.jianastrero.trainer.domain.enumeration
 
+import androidx.compose.ui.graphics.Color
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import org.jetbrains.compose.resources.DrawableResource
+import trainer.composeapp.generated.resources.Res
+import trainer.composeapp.generated.resources.bug
+import trainer.composeapp.generated.resources.compose_multiplatform
+import trainer.composeapp.generated.resources.dark
+import trainer.composeapp.generated.resources.dragon
+import trainer.composeapp.generated.resources.electric
+import trainer.composeapp.generated.resources.fairy
+import trainer.composeapp.generated.resources.fighting
+import trainer.composeapp.generated.resources.fire
+import trainer.composeapp.generated.resources.flying
+import trainer.composeapp.generated.resources.ghost
+import trainer.composeapp.generated.resources.grass
+import trainer.composeapp.generated.resources.ground
+import trainer.composeapp.generated.resources.ice
+import trainer.composeapp.generated.resources.normal
+import trainer.composeapp.generated.resources.poison
+import trainer.composeapp.generated.resources.psychic
+import trainer.composeapp.generated.resources.rock
+import trainer.composeapp.generated.resources.steel
+import trainer.composeapp.generated.resources.water
 
 @Serializable
-enum class PokemonType {
-    @SerialName("normal") Normal,
-    @SerialName("fighting") Fighting,
-    @SerialName("flying") Flying,
-    @SerialName("poison") Poison,
-    @SerialName("ground") Ground,
-    @SerialName("rock") Rock,
-    @SerialName("bug") Bug,
-    @SerialName("ghost") Ghost,
-    @SerialName("steel") Steel,
-    @SerialName("fire") Fire,
-    @SerialName("water") Water,
-    @SerialName("grass") Grass,
-    @SerialName("electric") Electric,
-    @SerialName("psychic")Psychic,
-    @SerialName("ice") Ice,
-    @SerialName("dragon") Dragon,
-    @SerialName("dark") Dark,
-    @SerialName("fairy") Fairy,
-    @SerialName("stellar") Stellar,
-    @SerialName("unknown") Unknown,
-    @SerialName("shadow") Shadow,
+enum class PokemonType(
+    val color: Color,
+    val icon: DrawableResource?,
+) {
+    @SerialName("normal") Normal(Color(0xFFA8A77A), Res.drawable.normal),
+    @SerialName("fighting") Fighting(Color(0xFFC22E28), Res.drawable.fighting),
+    @SerialName("flying") Flying(Color(0xFFA98FF3), Res.drawable.flying),
+    @SerialName("poison") Poison(Color(0xFFA33EA1), Res.drawable.poison),
+    @SerialName("ground") Ground(Color(0xFFE2BF65), Res.drawable.ground),
+    @SerialName("rock") Rock(Color(0xFFB6A136), Res.drawable.rock),
+    @SerialName("bug") Bug(Color(0xFFA6B91A), Res.drawable.bug),
+    @SerialName("ghost") Ghost(Color(0xFF735797), Res.drawable.ghost),
+    @SerialName("steel") Steel(Color(0xFFB7B7CE), Res.drawable.steel),
+    @SerialName("fire") Fire(Color(0xFFEE8130), Res.drawable.fire),
+    @SerialName("water") Water(Color(0xFF6390F0), Res.drawable.water),
+    @SerialName("grass") Grass(Color(0xFF7AC74C), Res.drawable.grass),
+    @SerialName("electric") Electric(Color(0xFFF7D02C), Res.drawable.electric),
+    @SerialName("psychic")Psychic(Color(0xFFF95587), Res.drawable.psychic),
+    @SerialName("ice") Ice(Color(0xFF96D9D6), Res.drawable.ice),
+    @SerialName("dragon") Dragon(Color(0xFF6F35FC), Res.drawable.dragon),
+    @SerialName("dark") Dark(Color(0xFF705746), Res.drawable.dark),
+    @SerialName("fairy") Fairy(Color(0xFFD685AD), Res.drawable.fairy),
+    @SerialName("stellar") Stellar(Color(0xFF46647E), null),
+    @SerialName("unknown") Unknown(Color(0xFF827E7A), null),
+    @SerialName("shadow") Shadow(Color(0xFF2B4463), null),
 }
-
