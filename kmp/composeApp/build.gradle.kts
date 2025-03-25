5ebf4108e3fff8bc2520a9346765d71b26a8d396
Jian James Astrero <jian.astrero@firstdigitalfinance.com>
2025-03-26 06:03:30 +0800

    Remove compose.uiTooling dependency from `composeApp` build file.

--- kmp/composeApp/build.gradle.kts
+++ kmp/composeApp/build.gradle.kts
@@ -40,7 +40,6 @@ kotlin {
             implementation(compose.material)
             implementation(compose.ui)
             implementation(compose.components.resources)
-            implementation(compose.uiTooling)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
