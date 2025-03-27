04867466ce260f0fc08752c46519baa3eed0c643
Jian James Astrero <jian.astrero@firstdigitalfinance.com>
2025-03-28 02:32:49 +0800

    Feat: Create `PokemonTypePills` molecule
    This commit introduces the `PokemonTypePills` molecule, a composable function that displays a list of Pokemon types as pills. It utilizes `FlowRow` to arrange the pills horizontally with spacing and wraps them to the next line if they exceed the available width. Each Pokemon type is rendered using the `PokemonTypePill` atom.

---
+++ kmp/composeApp/src/commonMain/kotlin/dev/jianastrero/trainer/ui/molecule/PokemonTypePills.kt
@@ -0,0 +1,47 @@
+package dev.jianastrero.trainer.ui.molecule
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.FlowRowOverflow
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import dev.jianastrero.trainer.domain.enumeration.PokemonType
+import dev.jianastrero.trainer.ui.atom.PokemonTypePill
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun PokemonTypePills(
+    pokemonTypes: List<PokemonType>,
+    modifier: Modifier = Modifier
+) {
+    FlowRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+        modifier = modifier
+    ) {
+        pokemonTypes.forEach {
+            PokemonTypePill(pokemonType = it)
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewPokemonTypePills() {
+    MaterialTheme {
+        PokemonTypePills(
+            pokemonTypes = PokemonType.entries,
+            modifier = Modifier
+                .background(MaterialTheme.colors.onBackground)
+                .padding(24.dp)
+        )
+    }
+}
