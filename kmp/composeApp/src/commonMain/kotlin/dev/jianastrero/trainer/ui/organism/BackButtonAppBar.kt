0381f3bbd2c539173a06038f824523882d52186f
Jian James Astrero <jian.astrero@firstdigitalfinance.com>
2025-03-29 01:15:18 +0800

    Feat: Improve BackButtonAppBar UI
    - Updates the UI of the `BackButtonAppBar` for better visual appearance and spacing.
    - Adjusts the padding and icon size within the `BackButtonAppBar` to improve touch target and visual balance.
    - Modifies the row arrangement to include spacing between elements and center vertical alignment.
    - Adds padding around the row to provide better spacing from the edges.

--- kmp/composeApp/src/commonMain/kotlin/dev/jianastrero/trainer/ui/organism/BackButtonAppBar.kt
+++ kmp/composeApp/src/commonMain/kotlin/dev/jianastrero/trainer/ui/organism/BackButtonAppBar.kt
@@ -1,6 +1,7 @@
 package dev.jianastrero.trainer.ui.organism
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
@@ -11,6 +12,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
@@ -24,14 +26,22 @@ fun BackButtonAppBar(
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
 ) {
-    Row(modifier = modifier) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .padding(
+                horizontal = 12.dp,
+                vertical = 8.dp
+            )
+    ) {
         Icon(
             imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
             contentDescription = "Back",
             tint = MaterialTheme.colors.onBackground,
             modifier = Modifier
-                .size(48.dp)
-                .padding(12.dp)
+                .size(32.dp)
+                .padding(4.dp)
                 .clip(CircleShape)
                 .clickable(enabled = enabled, onClick = onBack)
         )
